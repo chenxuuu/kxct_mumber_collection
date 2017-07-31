13725d35c7cb8ec733e27fb09dad4678ca4d6c3d
chenxuuu <lolicon@papapoi.com>
2017-07-31 21:13:53 +0800

    修正网站因https导致css与js被拦截问题

--- index.php
+++ index.php
@@ -37,10 +37,10 @@ if(isset($_SESSION['userid'])){
 window.location.href = 'http://www.chenxublog.com/explorer.html';</script>
 <![endif]-->
 <link rel="shortcut icon" href="favicon.ico">
-<link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/2.3.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/2.3.2/css/bootstrap.min.css">
 <link rel="stylesheet" href="css/bootstrap-responsive.min.css"><!--自适应要用到的css-->
-<script src="http://cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
-<script src="http://cdn.bootcss.com/bootstrap/2.3.2/js/bootstrap.min.js"></script>
+<script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
+<script src="//cdn.bootcss.com/bootstrap/2.3.2/js/bootstrap.min.js"></script>
 </head>
 <body>
 <div class="container-fluid">
